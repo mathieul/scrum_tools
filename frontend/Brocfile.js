cedf1c386d8f603eb1ecb9dc5abb858a705d6fe4
Mathieu Lajugie <mathieu@caring.com>
2015-04-14 23:18:56 -0700

    setup bootstrap and phoenix.js

--- frontend/Brocfile.js
+++ frontend/Brocfile.js
@@ -4,6 +4,14 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp();
 
-app.import('vendor/js/phoenix.js');
+// app.import('vendor/js/phoenix.js');
+
+var bootstrapDir = app.bowerDirectory + '/bootstrap-sass-official/assets';
+// select bootstrap JavaScript components to include
+var bootstrapComponents = ['dropdown', 'alert', 'transition'];
+
+for (var index in bootstrapComponents) {
+  app.import(bootstrapDir + '/javascripts/bootstrap/' + bootstrapComponents[index] + '.js');
+}
 
 module.exports = app.toTree();
