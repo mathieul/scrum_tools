43f3b4154b636b130be9ccbdf177429feaa41ad6
Mathieu Lajugie <mathieu@caring.com>
2015-04-18 10:00:55 -0700

    basic setup for daily task and section models

--- frontend/app/models/daily-section.js
+++ frontend/app/models/daily-section.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  label: DS.attr('string'),
-  position: DS.attr('number'),
-  tasks: DS.hasMany('daily-task')
+  label:     DS.attr('string'),
+  position:  DS.attr('number'),
+  tasks:     DS.hasMany('daily-task')
 });
