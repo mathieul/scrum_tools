95e7607ea328cd621c5ee6fc466468b04e5ef495
Mathieu Lajugie <mathieu@caring.com>
2015-04-19 08:38:21 -0700

    use fontawesome instead of bootstrap glyphicons

--- frontend/Brocfile.js
+++ frontend/Brocfile.js
@@ -1,12 +1,19 @@
 /* global require, module */
 
-var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var mergeTrees = require('broccoli-merge-trees');
+var pickFiles  = require('broccoli-static-compiler');
+var EmberApp   = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp();
 
-// app.import('vendor/js/phoenix.js');
+var fontAwesome = pickFiles(app.bowerDirectory + '/fontawesome/fonts', {
+    srcDir: '/',
+    destDir: '/assets/fonts'
+
+});
 
 var bootstrapDir = app.bowerDirectory + '/bootstrap-sass-official/assets';
+
 // select bootstrap JavaScript components to include
 var bootstrapComponents = ['dropdown', 'alert', 'transition', 'collapse'];
 
@@ -14,4 +21,4 @@ for (var index in bootstrapComponents) {
   app.import(bootstrapDir + '/javascripts/bootstrap/' + bootstrapComponents[index] + '.js');
 }
 
-module.exports = app.toTree();
+module.exports = mergeTrees([app.toTree(), fontAwesome]);
